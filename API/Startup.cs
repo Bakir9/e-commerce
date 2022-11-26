20254b8ed5454d987fa8fdc08e423c5ad410ec2a
bakir <bakir.malkoc@gmail.com>
2022-11-26 01:14:25 +0100

    API Paging, Filtering, Sorting & Searching

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,13 @@ namespace API
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +44,7 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
             app.UseAuthorization();
+            app.UseCors("CorsPolicy");
             app.UseSwaggerDocumentation();
             app.UseEndpoints(endpoints =>
             {
